0ec3d111d15040fe1e20bbee45965706d9c9781a
Nameer Rizvi <rizvinameer@gmail.com>
2023-06-03 14:05:20 -0400

    update eslint rules to latest ecma

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,16 @@ const eslintrc = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true,
+    es2022: true,
     node: true,
   },
   extends: ["eslint:recommended", "plugin:import/recommended"],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 14,
   },
-  rules: {},
+  rules: {
+    "no-inner-declarations": "off",
+  },
 };
 
 module.exports = eslintrc;
