a41addb586be215c32f2efb5dc07643e8f371761
Nameer Rizvi <rizvinameer@gmail.com>
2023-06-06 11:36:12 -0400

    refactoring root route

--- src/server/api/routes/root.js
+++ src/server/api/routes/root.js
@@ -1,13 +1,18 @@
 // --starterKit-flag [basically everything in this folder... Let's get busy!]
 const routes = require("../middlewares/routeManagerConfigs");
 
-const routeRoot = (req, res) =>
-  res.json({
-    name: "Application API",
-    description: "List of route addresses.",
-    routes: routes
-      .filter((r) => r.method === "GET")
-      .map((r) => ({ path: `/api${r.path}`, method: r.method })),
-  });
+function routeRoot(req, res) {
+  const name = "Node.js Boilerplate Application API";
+
+  const description = "List of route addresses.";
+
+  const _routes = routes
+    .filter((r) => r.method === "GET")
+    .map((r) => ({ path: `/api${r.path}`, method: r.method }));
+
+  const payload = { name, description, routes: _routes };
+
+  res.json(payload);
+}
 
 module.exports = routeRoot;
