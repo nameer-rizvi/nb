a4c286ab4820d9428d24943f5d9f8aedf8e5031a
Nameer Rizvi <rizvinameer@gmail.com>
2023-06-14 20:32:56 -0400

    logging middleware string

--- src/server/api/middlewares/logger.js
+++ src/server/api/middlewares/logger.js
@@ -3,18 +3,18 @@ const util = require("../../../util");
 // Log request information.
 
 function loggerMiddleware(req, res, next) {
-  util.log.route(`${req.method} ("${req.path}")`);
-
-  util.log.user(`Request by ${req.ip}`);
-
   const start = Date.now();
+
+  const path = req.path;
 
   res.on("finish", () => {
     const end = Date.now();
 
-    const duration = end - start;
+    const duration = (end - start).toLocaleString();
 
-    util.log.performance(`${duration.toLocaleString()} ms ("${req.path}")`);
+    const log = `${res.statusCode} ${req.method} ${path} by ${req.ip} took ${duration} ms.`;
+
+    util.log.route(log);
   });
 
   next();
