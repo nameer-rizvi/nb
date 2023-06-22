68004e4bbd0fc4ef5383e8f3560f8d08e574419c
Nameer Rizvi <rizvinameer@gmail.com>
2023-06-22 09:12:15 -0400

    colored status codes for logger

--- src/server/middlewares/logger.js
+++ src/server/middlewares/logger.js
@@ -8,11 +8,13 @@ function loggerMiddleware(req, res, next) {
   const path = req.path;
 
   res.on("finish", () => {
+    const statusCode = coloredStatusCode(res.statusCode);
+
     const end = Date.now();
 
     const duration = (end - start).toLocaleString();
 
-    const log = `${res.statusCode} ${req.method} ${path} by ${req.ip} took ${duration} ms.`;
+    const log = `${statusCode} ${req.method} ${path} by ${req.ip} took ${duration} ms.`;
 
     util.log.route(log);
   });
@@ -21,3 +23,13 @@ function loggerMiddleware(req, res, next) {
 }
 
 module.exports = loggerMiddleware;
+
+function coloredStatusCode(statusCode) {
+  if (statusCode < 300) {
+    return `\x1b[32m${statusCode}\x1b[0m`;
+  } else if (statusCode < 400) {
+    return `\x1b[36m${statusCode}\x1b[0m`;
+  } else if (statusCode < 500) {
+    return `\x1b[33m${statusCode}\x1b[0m`;
+  } else return `\x1b[31m${statusCode}\x1b[0m`;
+}
