d2244b4a1cdc4a8a7154db03bff6a78c7fa52e5b
nameer-rizvi <rizvinameer@gmail.com>
2025-02-09 12:54:32 -0500

    added issuer to jwt

--- src/util/jwt.js
+++ src/util/jwt.js
@@ -2,7 +2,11 @@ const jsonwebtoken = require("jsonwebtoken");
 const config = require("../config");
 const log = require("./log");
 
+exports.iss = new URL(config.urlWebsite || config.urlLocalhost).hostname;
+
 exports.sign = function signJWT(data = {}, expiresIn = 30) {
+  data.iss = exports.iss;
+
   const token = jsonwebtoken.sign(data, config.jwtSecret, { expiresIn });
 
   log.util("jwt signed");
@@ -13,7 +17,7 @@ exports.sign = function signJWT(data = {}, expiresIn = 30) {
 exports.verify = function verifyJWT(token) {
   const data = jsonwebtoken.verify(token, config.jwtSecret);
 
-  if (!data) throw new Error("Invalid token.");
+  if (!data || data.iss !== exports.iss) throw new Error("Invalid token.");
 
   log.util("jwt verified");
 
