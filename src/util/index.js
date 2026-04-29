43720e737eb62f1d4a3cb896220055ca067d1174
nameer-rizvi <rizvinameer@gmail.com>
2026-04-29 11:54:10 -0400

    redis client review

--- src/util/index.js
+++ src/util/index.js
@@ -7,11 +7,3 @@ const util = {
 };
 
 module.exports = util;
-
-util
-  .fetcher("https://jsonplaceholder.typicode.com/todos/1", {
-    throwError: false,
-    rateLimit: [10, 1000 * 60],
-  })
-  .then(console.log)
-  .catch(console.error);
