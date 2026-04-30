33c872e17a94329b1b6a948f81070164b21446f4
nameer-rizvi <rizvinameer@gmail.com>
2026-04-30 16:50:15 -0400

    /worker review

--- src/worker/_job.js
+++ src/worker/_job.js
@@ -1,25 +1,17 @@
 const util = require("../util");
 const database = require("../database");
 
-const index = {
-  generateSitemapUrls: require("./generateSitemapUrls"),
-};
-
 async function job(...jobs) {
   for (const name of jobs) {
-    if (index[name]) {
-      try {
-        util.log.job(`started ("${name}")`);
-        await index[name]();
-        util.log.job(`completed ("${name}")`);
-      } catch (error) {
-        util.log.job(`errored ("${name}"): ${error}`, "error");
-        await database.controller.error.add(error);
-      } finally {
-        util.log.job(`finished ("${name}")`);
-      }
-    } else {
-      util.log.job(`is undefined ("${name}")`, "warn");
+    try {
+      util.log.job(`started ("${name}")`);
+      await require(`./${name}`)();
+      util.log.job(`completed ("${name}")`);
+    } catch (error) {
+      util.log.job(`errored ("${name}"): ${error}`, "error");
+      await database.controller.error.add(error);
+    } finally {
+      util.log.job(`finished ("${name}")`);
     }
   }
 }
